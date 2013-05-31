1a44640798d10cc251dff82a0af781d05105dc8e
Rene Damm <rene@unity3d.com>
2013-05-31 18:12:00 +0200

    [Tests] FEATURE: Errors, asserts, and other logged conditions can now be EXPECT'd in native tests. [Logging] CHANGE: Removed EnableConsoleLogging() and DisableConsoleLogging() again; these were bad hacks.
    EXPECT (Error, "Thingy failed");
    CHECK_EQUAL (false, myThing->DoIt ("bad arg"));
    
    Anything unexpected will make a test fail so if you don't explicitly expect a specific
    assert, it'll make your test fail.
    
    --HG--
    branch : hackweek-2013-may/unittesting

--- src/TestDetails.h
+++ src/TestDetails.h
@@ -6,6 +6,12 @@ namespace UnitTest {
 class TestDetails
 {
 public:
+	TestDetails ()
+		: suiteName (0)
+		, testName (0)
+		, filename (0)
+		, lineNumber (-1) {}
+
     TestDetails(char const* testName, char const* suiteName, char const* filename, int lineNumber);
     TestDetails(const TestDetails& details, int lineNumber);
 
