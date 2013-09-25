f22ca10b05b4f518aff19cee28bec77bd8bfda21
Peter Kuhn <peter@unity3d.com>
2013-09-25 10:41:59 -0400

    Remove wii support
    --HG--
    branch : release/4.3/consoles/no_wii

--- src/TimeHelpers.h
+++ src/TimeHelpers.h
@@ -2,9 +2,6 @@
 
 #if defined UNITTEST_POSIX
     #include "Posix/TimeHelpers.h"
-// Unit-Tests don't include UnityConfigure.h, so we're using RVL define instead of UNITY_WII
-#elif defined(RVL)
-	#include "Wii/TimeHelpers.h"
 #elif defined(_MSC_VER)
     #include "Win32/TimeHelpers.h"
 #else
