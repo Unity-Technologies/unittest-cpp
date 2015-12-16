0a336a2a7d58621d9679c32a599cca00d7526e72
Alexey Zakharov <alexeyz@unity3d.com>
2015-12-16 12:45:38 +0100

    Added PERFORMANCE_TEST_LOOP_TIMELIMIT, decreased default time limit to 500ms
    --HG--
    branch : core/performance/native-performance-tests

--- src/ReportMacros.h
+++ src/ReportMacros.h
@@ -7,5 +7,7 @@
     #error UnitTest++ redefines REPORT_TEST_PROPERTY
 #endif
 
-#define REPORT_TEST_PROPERTY(NAME, VALUE)                                  \
-	do { UnitTest::CurrentTest::Results()->OnTestReportPropery(*UnitTest::CurrentTest::Details(), NAME, UnitTest::TestProperty(VALUE)); } while (0)
+// Add custom property to the current test result.
+// The property is a pair of string and UnitTest::TestProperty and will be a test reporter together with other test results.
+#define REPORT_TEST_PROPERTY(Name, Value) \
+	do { UnitTest::CurrentTest::Results()->OnTestReportPropery(*UnitTest::CurrentTest::Details(), Name, UnitTest::TestProperty(Value)); } while (0)
