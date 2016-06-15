871e7acae64e3aed6fed7363d331d75f7093a112
Richard Fine <richardf@unity3d.com>
2016-06-15 11:52:51 +0100

    Add comment to CheckStringsEqual explaining the pointer compare
    --HG--
    branch : core/reflectable-enums

--- src/Checks.cpp
+++ src/Checks.cpp
@@ -10,6 +10,7 @@ bool CheckStringsEqual(TestResults& results, char const* expected, char const* a
 {
 	using namespace std;
 
+	// Before calling strcmp, just compare the pointers - it may be an optimization, but it also means CHECK() for NULL strings can pass without crashing.
     if ((expected != actual) && strcmp(expected, actual))
     {
         UnitTest::MemoryOutStream stream;
