173353f05103dc915cc1d4f0103e53c2c024c931
Martin Sternevald <sternevald@gmail.com>
2018-02-05 09:29:52 +0100

    [unity] added baselib timehelpers

--- UnitTest++/TimeHelpers.h
+++ UnitTest++/TimeHelpers.h
@@ -1,9 +1,25 @@
 #include "Config.h"
 
-#if defined UNITTEST_POSIX
+#if defined (UNITTEST_BASELIB)
+    #include "TimeHelpersBaselib.h"
+#elif defined (UNITTEST_POSIX)
     #include "Posix/TimeHelpers.h"
 #elif defined(_MSC_VER)
     #include "Win32/TimeHelpers.h"
-#else
-    #include "TimeHelpersDefault.h"
+
+#else // dummy impl
+namespace UnitTest {
+
+    class Timer
+    {
+        Timer() {}
+        void Start() {}
+        double GetTimeInMs() const { return 0; }
+    }
+
+    namespace TimeHelpers
+    {
+        void SleepMs(int const ms) {}
+    }
+}
 #endif
